7d76d9410ab075e4a803415a2a9cebb36d5db325
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-15 22:17:04 +0530

    compilation

--- stddetail.java
+++ stddetail.java
@@ -14,7 +14,7 @@ public class stddetail{
       
       System.out.println("name:"+name);
       System.out.println("age:"+age);
-      System.out.println("cgpa:"+Math.floor(cgpa*100)/100);
+      System.out.println("cgpa:"+Math.floor(cgpa*100/100));
       System.out.println("grade:"+grade);
         scan.close();
     }
