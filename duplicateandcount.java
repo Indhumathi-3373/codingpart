6eb5ac9d99cc581a07b9896e543b7021a4a8372c
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-19 11:28:02 +0530

    2d array

---
+++ duplicateandcount.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class duplicateandcount {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int count = 0;
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) arr[i] = scan.nextInt();
+        for (int i = 0; i < size; i++) {
+            for(int j=i+1;j<size;j++)
+                if (arr[i] == arr[j]) {
+                   arr[j]=-1;
+                } 
+        }
+        for(int i=0;i<size;i++){
+            if(arr[i]!=-1){
+                count++;
+                System.out.print("\ndistinct array value:"+arr[i]+" ");
+            }
+        }
+        System.out.println("\ncount of distinct element :"+(count));scan.close();
+
+    }
+}
+
--- uniqueelement.java
+++
@@ -1,5 +0,0 @@
-public class uniqueelement {
-    public static void main(String[] args) {
-        SCa
-    }
-}
