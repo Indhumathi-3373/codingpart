ac1d8070fd94b4cbecc0aff5e7789efce798d1e4
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-25 12:36:30 +0530

    anagram

--- fibonacci.java
+++ fibonacci.java
@@ -2,16 +2,16 @@
 import java.util.*;
 public class fibonacci {
     static int fibo(int num,int fact){
-        if(num==0 || num==1 && fact<=num){
-            return 1;
+        if(num==0 || num==1){
+            return num;
         }else{
-            return fact+fibo(num-1,fact);
+            return fibo(num-1)+fibo(num-2);
         }
     }
     public static void main(String args[]){
         Scanner in=new Scanner(System.in);
         int num=in.nextInt();
         int fact=in.nextInt();
-        System.out.println(fibo(num,fact));
+        System.out.println(fibo(num,fact));in.close();
     }
 }
