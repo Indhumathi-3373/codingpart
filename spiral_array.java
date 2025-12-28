d30abcf5e00be4602d50976d0246fb9ddb392e19
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-28 16:13:45 +0530

    spiral array

--- spiral_array.java
+++ spiral_array.java
@@ -2,22 +2,26 @@ import java.util.*;
 public class spiral_array{
     public static void main(String args[]){
      Scanner scan=new Scanner(System.in);
-    int n1=scan.nextInt();
-    int arr1[][]=new int[n1][n1];
+    int n=scan.nextInt();
+    
+    int arr1[][]=new int[n][n];
     System.out.println("enter elements for array :");
-    for(int i=0;i<n1;i++){
-         for(int j=0;j<n1;j++){
+    for(int i=0;i<n;i++){
+         for(int j=0;j<n;j++){
              arr1[i][j]=scan.nextInt();
             }
-    }for(int i=0;i<1;i++){
-        for(int j=i;j<n1;j++){
-                System.out.print(arr1[i][j]);
-        }for(int j=1;j<n1;j++){
-              System.out.print(arr1[j][n1-i-1]);
-            }
-        for(int j=n1-1;j<n1;j--){
-
-        }//not complete yet
+    }
+    double r=Math.ceil(((double)n/2));
+    for(int i=0;i<r;i++){
+        for(int j=i;j<=n-1-i;j++){
+                System.out.print(arr1[i][j]+" ");
+         }for(int j=1+i;j<=n-i-1;j++){
+            System.out.print(arr1[j][n-1-i]+" ");
+         }for(int j=n-i-2;j>=i;j--){
+             System.out.print(arr1[n-i-1][j]+" ");
+         }for(int j=n-i-2;j>i;j--){
+            System.out.print(arr1[j][i]+" ");
+         }
         }scan.close();
-    }
-    }
+    }}
+    
