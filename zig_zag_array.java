b76ab89b9beda0518da3bd940bd5fa36c47aeeb1
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-31 12:43:22 +0530

    zig zag array

--- zig_zag_array.java
+++ zig_zag_array.java
@@ -1,16 +1,26 @@
 import java.util.*;
+
 public class zig_zag_array {
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
         System.out.println("enter size of an array :");
         int size=scan.nextInt();
-        int arr[]=new int[size];
+        int arr[][]=new int[size][size];
         System.out.println("enter element of an array :");
         for(int i=0;i<size;i++){
-            arr[i]=scan.nextInt();
+            for(int j=0;j<size;j++){
+              arr[i][j]=scan.nextInt();}
         }
         for(int i=0;i<size;i++){
-            //not yet completed
+          System.out.print(arr[0][i]);   
+        }     
+        for(int j=1;j<size-1;j++){
+            System.out.print(arr[j][size-j-1]);
+        }   
+         for(int j=0;j<size;j++){
+            System.out.print(arr[size-1][j]);
+        }
+       
+scan.close();
         }
     }
-}
