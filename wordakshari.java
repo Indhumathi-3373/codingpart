fd9ac6f3a57e4bade151b83c1a7cfa714c2f4b1f
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-31 16:24:58 +0530

    wordakshari

--- wordakshari.java
+++ wordakshari.java
@@ -2,13 +2,24 @@ import java.util.*;
 public class wordakshari {
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
+        System.out.println("PRINT #### TO EXIT");
+        System.out.println("GAME STARTED.....");
         String input=scan.nextLine();
-        String b="";
         String exit="####";
+        System.out.println("entered input : "+input);
         while(!input.equals(exit)){
-            System.out.println(input);
+            char[] ch=input.toCharArray();
             String input1=scan.nextLine();
-          
+            char ch2[]=input1.toCharArray();
+            if(ch2[0] != ch[ch.length-1]){
+            System.out.println("----------GAME OVER----------");
+                break; 
+            }else{
+                System.out.println("input matched... "+input1+" enter next input");
+               input=input1;
+               
+            }
+            
         }
 scan.close();
     }
