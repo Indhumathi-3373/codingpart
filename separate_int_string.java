717fe187d12d7c847565d8bc10ecb748b897eb9c
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-31 16:43:59 +0530

    addition of the extracted value

--- separate_int_string.java
+++ separate_int_string.java
@@ -5,8 +5,16 @@ public static void main(String args[]){
     String input=scan.nextLine().toLowerCase();;
     String text=input.replaceAll("[0-9]","");
     String num=input.replaceAll("[a-z]","");
+    int sum=0;
+    int number=Integer.parseInt(num);
     System.out.println(input);
     System.out.println("after ignoring numbers input will be : "+text);
-    System.out.println("after ignoring alphabets input will be : "+num);scan.close();
+    System.out.println("after ignoring alphabets input will be : "+number);
+    while(number>0){
+        int x=number%10;
+        sum=sum+x;
+        number=number/10;
+    }System.out.println("ADDITION OF THE EXTRACTED NUMBER : "+sum);
+    scan.close();
 }    
 }
