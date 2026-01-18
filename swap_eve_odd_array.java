3dbcd0c5a360b1e33f9952322a91ffdfe148af78
indhumathi-3373 <indhumathi93428@gmail.com>
2026-01-18 17:50:27 +0530

    arrange even first then odd (swap from first to last )

---
+++ swap_eve_odd_array.java
@@ -0,0 +1,31 @@
+//example size 8 array 10 98 3 33 12 22 21 11
+//first two is even 3rd is odd checking from last for any even value available if yes means swap  3 to that even value
+//output should be 10 98 22 12 33 3 21 11
+import java.io.*;
+import java.util.*;
+
+public class swap_eve_odd_array {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];int left=0,right=size-1;
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array after Segregation");
+        while(left<right){
+            if(arr[left]%2==0){
+                left++;
+            }else if(arr[right]%2==0){
+                int temp=arr[right];
+                arr[right]=arr[left];
+                arr[left]=temp;
+            }else{
+                right--;
+            }
+        }for(int i=0;i<size;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
