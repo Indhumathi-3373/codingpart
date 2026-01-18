bdb3ae2f666c2bcf444abce7ef2156c8c8d1feb5
indhumathi-3373 <indhumathi93428@gmail.com>
2026-01-18 21:19:43 +0530

    finding max element of each column

---
+++ max_element_eachcolumn.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class max_element_eachcolumn {
+
+    public static void main(String[] args) {
+           Scanner sc=new Scanner(System.in);
+    int n1=sc.nextInt();
+   int n2=sc.nextInt();
+    int arr1[][]=new int[n1][n2];
+    for(int i=0;i<n1;i++){
+         for(int j=0;j<n2;j++){
+             arr1[i][j]=sc.nextInt();
+         }
+    }
+    for(int i=0;i<n2;i++){
+         int temp=0;
+        for(int j=0;j<n1;j++){
+            if(arr1[j][i]>temp){
+                temp=arr1[j][i];
+            }
+        }System.out.println(temp);
+    }
+
+    }
+}
