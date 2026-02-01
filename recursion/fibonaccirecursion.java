f2c62678a9709771d9191aefeafe6172958a1079
indhumathi-3373 <indhumathi93428@gmail.com>
2026-02-01 20:26:56 +0530

    fibonacci recursion

--- recursion/fibonaccirecursion.java
+++ recursion/fibonaccirecursion.java
@@ -2,17 +2,24 @@ package recursion;
 
 import java.util.*;
 public class fibonaccirecursion {
-    static int fibo(int num){
-        if(num==0 || num==1){
-            return num;
-        }else{
-            return fibo(num-1)+fibo(num-2);
-        }
-    }
-    public static void main(String args[]){
-        Scanner in=new Scanner(System.in);
-        int num=in.nextInt();
-        for(int i=0;i<=num;i++)
-          System.out.print(fibo(i));in.close();
+
+
+
+       static int fibo(int num){
+           if( num==1 ){
+               return 0;
+           }
+          else if( num==2){
+              return 1;
+          }
+               return fibo(num-1)+fibo(num-2);
+           
+       }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        int num=sc.nextInt();
+        System.out.println("The term "+num+" in the Fibonacci series is "+fibo(num));sc.close();
     }
 }
+
